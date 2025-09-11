ec064acdee0a3b51a349f9fdf02728ec26404086
bergercasey <caseyb@advanced-mechanical.com>
2025-09-10 20:02:39 -0500

    Delete netlify/functions directory

--- netlify/functions/netlify/functions/auth-logout.js
+++
@@ -1,5 +0,0 @@
-// netlify/functions/auth-logout.js (ESM)
-export async function handler() {
-  const cookie = ['binder_auth=deleted','Path=/','HttpOnly','SameSite=Lax','Secure','Max-Age=0'].join('; ');
-  return { statusCode: 200, headers: { 'Set-Cookie': cookie, 'Cache-Control':'no-store' }, body: 'OK' };
-}
